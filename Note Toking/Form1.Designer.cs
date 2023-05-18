54547000fba3e98cadc928cb7829fc7e9118a668
KingdomahaCodev363 <bvikash363@gmail.com>
2023-05-18 16:16:59 +0530

    Add project files.

---
+++ Note Toking/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Note_Toking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.texttital = new Guna.UI2.WinForms.Guna2TextBox();
+            this.textmassage = new Guna.UI2.WinForms.Guna2TextBox();
+            this.DataGridView = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.bttnew = new Guna.UI2.WinForms.Guna2Button();
+            this.bttsave = new Guna.UI2.WinForms.Guna2Button();
+            this.bttread = new Guna.UI2.WinForms.Guna2Button();
+            this.bttdelete = new Guna.UI2.WinForms.Guna2Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.AutoSize = false;
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(114, 141);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(45, 18);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Tital";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.AutoSize = false;
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(99, 271);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(60, 18);
+            this.guna2HtmlLabel2.TabIndex = 1;
+            this.guna2HtmlLabel2.Text = "Massage";
+            // 
+            // texttital
+            // 
+            this.texttital.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.texttital.DefaultText = "";
+            this.texttital.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.texttital.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.texttital.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.texttital.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.texttital.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.texttital.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.texttital.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.texttital.Location = new System.Drawing.Point(165, 141);
+            this.texttital.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.texttital.Name = "texttital";
+            this.texttital.PasswordChar = '\0';
+            this.texttital.PlaceholderText = "";
+            this.texttital.SelectedText = "";
+            this.texttital.Size = new System.Drawing.Size(229, 48);
+            this.texttital.TabIndex = 2;
+            // 
+            // textmassage
+            // 
+            this.textmassage.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textmassage.DefaultText = "";
+            this.textmassage.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.textmassage.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.textmassage.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textmassage.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.textmassage.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textmassage.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.textmassage.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.textmassage.Location = new System.Drawing.Point(165, 253);
+            this.textmassage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textmassage.Name = "textmassage";
+            this.textmassage.PasswordChar = '\0';
+            this.textmassage.PlaceholderText = "";
+            this.textmassage.SelectedText = "";
+            this.textmassage.Size = new System.Drawing.Size(397, 274);
+            this.textmassage.TabIndex = 3;
+            // 
+            // DataGridView
+            // 
+            this.DataGridView.AllowUserToAddRows = false;
+            this.DataGridView.AllowUserToDeleteRows = false;
+            this.DataGridView.AllowUserToResizeColumns = false;
+            this.DataGridView.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.DataGridView.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.DataGridView.ColumnHeadersHeight = 4;
+            this.DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DataGridView.DefaultCellStyle = dataGridViewCellStyle3;
+            this.DataGridView.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.DataGridView.Location = new System.Drawing.Point(638, 141);
+            this.DataGridView.Name = "DataGridView";
+            this.DataGridView.ReadOnly = true;
+            this.DataGridView.RowHeadersVisible = false;
+            this.DataGridView.RowHeadersWidth = 51;
+            this.DataGridView.RowTemplate.Height = 24;
+            this.DataGridView.Size = new System.Drawing.Size(289, 386);
+            this.DataGridView.TabIndex = 4;
+            this.DataGridView.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.DataGridView.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.DataGridView.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.DataGridView.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.DataGridView.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.DataGridView.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.DataGridView.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.DataGridView.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.DataGridView.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.DataGridView.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DataGridView.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.DataGridView.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.DataGridView.ThemeStyle.HeaderStyle.Height = 4;
+            this.DataGridView.ThemeStyle.ReadOnly = true;
+            this.DataGridView.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.DataGridView.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.DataGridView.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DataGridView.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.DataGridView.ThemeStyle.RowsStyle.Height = 24;
+            this.DataGridView.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.DataGridView.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // bttnew
+            // 
+            this.bttnew.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bttnew.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bttnew.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bttnew.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bttnew.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bttnew.ForeColor = System.Drawing.Color.White;
+            this.bttnew.Location = new System.Drawing.Point(165, 572);
+            this.bttnew.Name = "bttnew";
+            this.bttnew.Size = new System.Drawing.Size(125, 45);
+            this.bttnew.TabIndex = 5;
+            this.bttnew.Text = "New";
+            this.bttnew.Click += new System.EventHandler(this.bttnew_Click);
+            // 
+            // bttsave
+            // 
+            this.bttsave.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bttsave.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bttsave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bttsave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bttsave.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bttsave.ForeColor = System.Drawing.Color.White;
+            this.bttsave.Location = new System.Drawing.Point(389, 572);
+            this.bttsave.Name = "bttsave";
+            this.bttsave.Size = new System.Drawing.Size(125, 45);
+            this.bttsave.TabIndex = 6;
+            this.bttsave.Text = "Save";
+            this.bttsave.Click += new System.EventHandler(this.bttsave_Click);
+            // 
+            // bttread
+            // 
+            this.bttread.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bttread.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bttread.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bttread.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bttread.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bttread.ForeColor = System.Drawing.Color.White;
+            this.bttread.Location = new System.Drawing.Point(595, 572);
+            this.bttread.Name = "bttread";
+            this.bttread.Size = new System.Drawing.Size(125, 45);
+            this.bttread.TabIndex = 7;
+            this.bttread.Text = "Read";
+            this.bttread.Click += new System.EventHandler(this.bttread_Click);
+            // 
+            // bttdelete
+            // 
+            this.bttdelete.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bttdelete.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bttdelete.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bttdelete.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bttdelete.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.bttdelete.ForeColor = System.Drawing.Color.White;
+            this.bttdelete.Location = new System.Drawing.Point(802, 572);
+            this.bttdelete.Name = "bttdelete";
+            this.bttdelete.Size = new System.Drawing.Size(125, 45);
+            this.bttdelete.TabIndex = 8;
+            this.bttdelete.Text = "Delete";
+            this.bttdelete.Click += new System.EventHandler(this.bttdelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(964, 723);
+            this.Controls.Add(this.bttdelete);
+            this.Controls.Add(this.bttread);
+            this.Controls.Add(this.bttsave);
+            this.Controls.Add(this.bttnew);
+            this.Controls.Add(this.DataGridView);
+            this.Controls.Add(this.textmassage);
+            this.Controls.Add(this.texttital);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox texttital;
+        private Guna.UI2.WinForms.Guna2TextBox textmassage;
+        private Guna.UI2.WinForms.Guna2DataGridView DataGridView;
+        private Guna.UI2.WinForms.Guna2Button bttnew;
+        private Guna.UI2.WinForms.Guna2Button bttsave;
+        private Guna.UI2.WinForms.Guna2Button bttread;
+        private Guna.UI2.WinForms.Guna2Button bttdelete;
+    }
+}
+
---
+++ Note Toking/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Toking
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+      
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("title", typeof(String));
+            table.Columns.Add("massage", typeof(String));
+            DataGridView.DataSource = table;
+
+            DataGridView.Columns["Massage"].Visible = false;
+         
+        }
+
+        private void bttnew_Click(object sender, EventArgs e)
+        {
+            texttital.Clear();
+            textmassage.Clear();
+        }
+
+        private void bttsave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(texttital.Text,textmassage.Text);
+
+            texttital.Clear();
+            textmassage.Clear();
+        }
+
+        private void bttread_Click(object sender, EventArgs e)
+        {
+            int index = DataGridView.CurrentCell.RowIndex;
+
+            if(index > -1)
+            {
+                texttital.Text = table.Rows[index].ItemArray[0].ToString();  
+                textmassage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void bttdelete_Click(object sender, EventArgs e)
+        {
+            int index = DataGridView.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+        }
+    }
+}
